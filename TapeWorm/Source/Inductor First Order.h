96e9552749b283c039f7919933bb00438d13c764
Tim Leete <tim.leete@spicechateaudsp.com>
2025-01-26 23:19:28 -0600

    Refactor inductor

--- TapeWorm/Source/Inductor First Order.h
+++ TapeWorm/Source/Inductor First Order.h
@@ -11,40 +11,30 @@
 #pragma once
 #include <JuceHeader.h>
 
-enum FilterType {
-    HPF,
-    LPF
-};
-
 class Inductor_FirstOrder {
 public:
-    Inductor_FirstOrder (juce::AudioProcessorValueTreeState&, FilterType type, bool variable);
+    enum FilterType {
+        HPF,
+        LPF
+    };
+    
+    Inductor_FirstOrder (FilterType type);
     ~Inductor_FirstOrder () {};
     
-    void prepare (int new_numChannels, int new_bufferSize, double new_sampleRate);
+    void prepare (const float new_sampleRate, const int new_bufferSize);
     void reset ();
     
-    void processBuffer (juce::AudioBuffer<float>& buffer);
-    void processBlock (juce::dsp::AudioBlock<float> block);
-    double processSample (int channel, double sample);
-    std::pair<double, double> processSample_stereo (std::pair<double, double> input);
+    double processSample (int channelIndex, double sample);
+    void smoothParameters (int channelIndex);
     
-    void setFilterType (FilterType newType) { type = newType; }
-    
-    void updateParameters_sample (bool forceUpdate = false);
-    void updateParameters_buffer (bool forceUpdate = false);
-    
-    void smoothParameters (int channel);
+    void setFilterType (FilterType newType);
+    void setPotentiometer (const float newP);
     
 private:
-    juce::AudioProcessorValueTreeState& apvts;
-    
     FilterType type;
     
-    double Fs = 48000;
-    double Ts = 1 / Fs;
-    int numChannels = 2;
-    int bufferSize = 128;
+    double sampleRate = 48000;
+    double samplePeriod = 1 / sampleRate;
     
     double Ui = 400;
     const double Uo = 4 * juce::MathConstants<double>::pi * 10e-7;
@@ -57,22 +47,16 @@ private:
     double b = (1 - std::sqrt (1 / Ui)) / Bsat;
     double c = 1 / u;
     
-    struct {
-        std::atomic<float>* P_vts;
-    } valueTree;
-    
-    float P_target = -1.f;
-    double P_smooth[2] {0.0};
+    double P_target = -1.f;
+    double P_smooth[2] { 0.0 };
     
     double R = 400e3;
     
     double x1[2] {0};
     double y1[2] {0};
     
-    bool isVariable = true;
-    
     float smoothTime_seconds = 0.1f; // 100 ms
-    double smooth_alpha = std::exp (-1 * std::log (9) / (Fs * smoothTime_seconds));
+    double smooth_alpha = std::exp (-1 * std::log (9) / (sampleRate * smoothTime_seconds));
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Inductor_FirstOrder);
 };
